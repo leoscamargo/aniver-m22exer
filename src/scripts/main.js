80f0d6b289b0cab0c47ae83ba3f86b79b0691e37
Leonardo <leogne98@hotmail.com>
2023-11-02 19:04:28 -0300

    alteração projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const dataDoEvento = new Date("Dec 10, 2023 23:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function(){
